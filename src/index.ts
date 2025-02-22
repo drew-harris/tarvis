4d713143b900f73e3cceeca4a5ba3fbcc250a913
Drew Harris <dsharris10@gmail.com>
2025-02-22 16:34:36 -0600

    change model

--- src/index.ts
+++ src/index.ts
@@ -54,7 +54,7 @@ client.once(Events.ClientReady, (readyClient) => {
 
 const handleTarvisMessage = async (message: Message) => {
   const result = await generateText({
-    model: openrouter("google/gemini-2.0-flash-lite-preview-02-05:free"),
+    model: openrouter("google/gemini-2.0-flash-001"),
     tools,
     system:
       "You are a discord bot that makes tool calls to accomplish tasks. If you don't find any tools to be useful then just respond normally. Also if a tool call doesn't have a useful result, such as null or undefined then don't speak again. If a user says 'show me' or 'send up', that also means they wanna see a gif. If a user asks a question that isn't a basic knowledge or obvious answer, use the recall too to gather more infomation before you answer.",
@@ -64,7 +64,6 @@ const handleTarvisMessage = async (message: Message) => {
   console.log(result.text);
   if (result.text.length > 4) {
     const message = messageStorage.getStore();
-
     message?.reply(result.text);
   }
 };
