6c389fd0938ff865bba68b72119163acd0cf57e5
Drew Harris <dsharris10@gmail.com>
2025-02-22 02:29:34 -0600

    fix double remember

--- src/index.ts
+++ src/index.ts
@@ -62,12 +62,11 @@ const handleTarvisMessage = async (message: Message) => {
     maxSteps: 2,
   });
   console.log(result.text);
+  if (result.toolCalls.some((t) => t.toolName === "remember")) {
+    return;
+  }
   if (result.text.length > 4) {
     const message = messageStorage.getStore();
-
-    if (result.toolCalls.some((t) => t.toolName === "remember")) {
-      return;
-    }
 
     message?.reply(result.text);
   }
